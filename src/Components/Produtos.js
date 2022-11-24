0ab281498e31cbd99e8b3989bb03486f904eb070
Luiz Serão <luiz.asdc@hotmail.com>
2022-11-24 14:28:04 -0300

    fetch com async function no Produtos.js

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -6,11 +6,13 @@ import styles from "./Produtos.module.css"
 const Produtos = () => {
   const [produtos, setProdutos] = React.useState(null)
 
-
   React.useEffect(() => {
-    fetch("https://ranekapi.origamid.dev/json/api/produto")
-    .then((response) => response.json())
-    .then((json) => setProdutos(json) )
+    const pushProducts = async function() {
+      const response = await fetch("https://ranekapi.origamid.dev/json/api/produto")
+      const json = await response.json()
+      setProdutos(json)
+    }
+    pushProducts()
   },[])
 
   console.log(produtos)
