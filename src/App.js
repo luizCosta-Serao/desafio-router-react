4e19f641fdf421db591c4d3a20f858634f3d900a
Luiz Serão <luiz.asdc@hotmail.com>
2022-11-24 14:03:59 -0300

    correção path do router v2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Produtos/>}/>
             <Route path="desafio-router-react/" element={<Produtos/>}/>
             <Route path="produto/:id" element={<Produto/>}/>
             <Route path="contato" element={<Contato />} />
